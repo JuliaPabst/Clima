8a7f2d5f2acd7c643cf90b66cd08101074647201
Jules <ju.pabst@gmail.com>
2024-01-11 16:12:38 +0100

    created XCode project

---
+++ Clima/Clima/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Julia Pabst on 11.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
