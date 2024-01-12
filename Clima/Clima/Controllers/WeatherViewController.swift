208c94e79596a08e1b05aaca09813622dca6f474
Jules <ju.pabst@gmail.com>
2024-01-12 15:07:02 +0100

    added switch to °F button

--- Clima/Clima/Controllers/WeatherViewController.swift
+++ Clima/Clima/Controllers/WeatherViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class WeatherViewController: UIViewController {
 
+    @IBOutlet var conditionImageView: UIImageView!
+    @IBOutlet var temperatureLabel: UILabel!
+    @IBOutlet var cityLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
