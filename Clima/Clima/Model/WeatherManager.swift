ec2e51807a716e4f6194084b5c86336a73556007
Jules <ju.pabst@gmail.com>
2024-01-14 16:42:29 +0100

    added conditionId, cityName and temperature to WeatherModel. Moved getConditionName to WeatherModel

--- Clima/Clima/Model/WeatherManager.swift
+++ Clima/Clima/Model/WeatherManager.swift
@@ -41,22 +41,6 @@ struct WeatherManager {
         } catch {
             print(error)
         }
-    }
-    
-    func getConditionName(id: Int) -> String {
-        switch id {
-        case 200 ... 232, 771: return "cloud.bolt.rain"
-        case 300 ... 321: return "cloud.drizzle"
-        case 500 ... 531: return "cloud.rain"
-        case 600 ... 622: return "cloud.snow"
-        case 701 ... 762: return "cloud.fog"
-        case 781: return "tornado"
-        case 800: return "sun.max"
-        case 801: return "cloud.sun"
-        case 802 ... 804: return "cloud.bolt"
-        default: return "cloud"
-        }
-    }
-    
+    }    
     
 }
--- Clima/Clima/Model/WeatherModel.swift
+++ Clima/Clima/Model/WeatherModel.swift
@@ -8,5 +8,23 @@
 import Foundation
 
 struct WeatherModel {
+    let conditionId: Int
+    let cityName: String
+    let temperature: Double
     
+    
+    func getConditionName(id: Int) -> String {
+        switch id {
+        case 200 ... 232, 771: return "cloud.bolt.rain"
+        case 300 ... 321: return "cloud.drizzle"
+        case 500 ... 531: return "cloud.rain"
+        case 600 ... 622: return "cloud.snow"
+        case 701 ... 762: return "cloud.fog"
+        case 781: return "tornado"
+        case 800: return "sun.max"
+        case 801: return "cloud.sun"
+        case 802 ... 804: return "cloud.bolt"
+        default: return "cloud"
+        }
+    }
 }
